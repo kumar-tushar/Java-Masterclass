1370ce489e4066f2b23457d904cb8d6d10a7d61f
kumar-tushar <kumartushar0110@gmail.com>
2021-04-04 04:39:43 +0530

    4 Apr 21 4:40 PM

--- ByteShortIntLong.java
+++ ByteShortIntLong.java
@@ -2,6 +2,30 @@ package com.company;
 
 public class ByteShortIntLong {
     public static void main(String[] args){
+        int myMinIntValue=Integer.MIN_VALUE;
+        int myMaxIntValue=Integer.MAX_VALUE;
+        System.out.println("Minimum Value "+myMinIntValue);
+        System.out.println("Maximum Value "+myMaxIntValue);
+
+        byte myMinByteValue=Byte.MIN_VALUE;
+        byte myMaxByteValue=Byte.MAX_VALUE;
+        System.out.println("Minimum Value "+myMinByteValue);
+        System.out.println("Maximum Value "+myMaxByteValue);
+
+        short myMinShortValue=Short.MIN_VALUE;
+        short myMaxShortValue=Short.MAX_VALUE;
+        System.out.println("Minimum Value "+myMinShortValue);
+        System.out.println("Maximum Value "+myMaxShortValue);
+
+        long myMinLongValue=Long.MIN_VALUE;
+        long myMaxLongValue=Long.MAX_VALUE;
+        System.out.println("Minimum Value "+myMinLongValue);
+        System.out.println("Maximum Value "+myMaxLongValue);
+
+        long number=21474836478L;
+        System.out.println(number);
+
+
 
     }
 }
