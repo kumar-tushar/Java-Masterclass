d20ccb4e4f51a12f55c153f22acee51f38c3779c
kumar-tushar <kumartushar0110@gmail.com>
2021-04-18 06:45:11 +0530

    done

---
+++ FloatAndDouble.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class FloatAndDouble {
+    public static void main(String[] args){
+        
+        float myMinFloatValue=Float.MIN_VALUE;
+        float myMaxFloatValue=Float.MAX_VALUE;
+        System.out.println(myMinFloatValue);
+        System.out.println(myMaxFloatValue);
+
+        double myMinDoubleValue=Double.MIN_VALUE;
+        double myMaxDoubleValue=Double.MAX_VALUE;
+        System.out.println(myMinDoubleValue);
+        System.out.println(myMaxDoubleValue);
+
+    }
+}
