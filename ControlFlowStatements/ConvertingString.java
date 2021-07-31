34d4f3c0e9dcefc2a98d0e27326fc8817b358fdb
kumar-tushar <kumartushar0110@gmail.com>
2021-08-01 00:31:42 +0530

    01/08

---
+++ ControlFlowStatements/ConvertingString.java
@@ -0,0 +1,16 @@
+package com.company.ControlFlowStatements;
+
+public class ConvertingString {
+
+    public static void main(String[] args){
+
+        String s="2021";
+
+        int i=Integer.parseInt(s);
+        System.out.println(i);
+
+        double d=Double.parseDouble(s);
+        System.out.println(d);
+
+    }
+}
