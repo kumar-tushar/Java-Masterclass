7fc473c485405d5319c290b9ce7f17d4d41546fd
kumar-tushar <kumartushar0110@gmail.com>
2021-07-31 17:18:40 +0530

    31/07

--- FirstSteps/ByteShortIntLong.java
+++ FirstSteps/ByteShortIntLong.java
@@ -4,11 +4,6 @@ public class ByteShortIntLong {
 
     public static void main(String[] args){
 
-        int myMinIntValue=Integer.MIN_VALUE;
-        int myMaxIntValue=Integer.MAX_VALUE;
-        System.out.println("Minimum Value "+myMinIntValue);
-        System.out.println("Maximum Value "+myMaxIntValue);
-
         byte myMinByteValue=Byte.MIN_VALUE;
         byte myMaxByteValue=Byte.MAX_VALUE;
         System.out.println("Minimum Value "+myMinByteValue);
@@ -18,6 +13,11 @@ public class ByteShortIntLong {
         short myMaxShortValue=Short.MAX_VALUE;
         System.out.println("Minimum Value "+myMinShortValue);
         System.out.println("Maximum Value "+myMaxShortValue);
+
+        int myMinIntValue=Integer.MIN_VALUE;
+        int myMaxIntValue=Integer.MAX_VALUE;
+        System.out.println("Minimum Value "+myMinIntValue);
+        System.out.println("Maximum Value "+myMaxIntValue);
 
         long myMinLongValue=Long.MIN_VALUE;
         long myMaxLongValue=Long.MAX_VALUE;
---
+++ FirstSteps/Char.java
@@ -0,0 +1,13 @@
+package com.company.FirstSteps;
+
+public class Char {
+
+    public static void main(String[] args){
+
+        char myChar='D';
+        System.out.println(myChar);
+
+        char myUnicodeChar='\u00A9';
+        System.out.println(myUnicodeChar);
+    }
+}
