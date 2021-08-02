49df6e8134b7f1875f85e420c39176e136358fd2
kumar-tushar <kumartushar0110@gmail.com>
2021-08-02 12:24:15 +0530

    02/08

--- OOP/Car/Main.java
+++ OOP/Car/Main.java
@@ -3,6 +3,7 @@ package com.company.OOP.Car;
 public class Main {
 
     public static void main(String[] args){
+
         Car porsche=new Car();
         System.out.println(porsche.getModel());
         System.out.println(porsche.getColour());
